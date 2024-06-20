682e0fdcd5231f308fa97e707d78fc0cc39e1b09
YasminB26 <141245280+YasminB26@users.noreply.github.com>
2024-06-20 12:28:47 +0000

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -28,7 +28,7 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'arrow-back-circle' : 'arrow-redo-circle-outline'} color={color} />
           ),
         }}
       />
